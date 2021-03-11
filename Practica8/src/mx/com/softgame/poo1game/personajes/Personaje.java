e78f2b654aec899f6e7fac13fc1602be3954a5cc
AlineZavala-19 <79319893+AlineZavala-19@users.noreply.github.com>
2021-03-11 17:49:19 -0600

    Create Personaje.java

---
+++ Practica8/src/mx/com/softgame/poo1game/personajes/Personaje.java
@@ -0,0 +1,48 @@
+package mx.com.softgame.poo1game.personajes;
+public class Personaje{
+	//atributos
+	private String nombre;
+	private int vida;
+	//constructores
+	public Personaje (String nombre, int vida){
+		this.nombre = nombre;
+		this.vida = vida;
+	}
+	public Personaje (String nombre){
+		this(nombre,3);
+	}
+	//metodos
+	public String getNombre(){
+		return nombre;
+	}
+	public int getVida(){
+		return vida;
+	}
+	public String getDetalle(){
+		return nombre + "\t" + vida; 
+	}
+	public void setNombre(String nombre){
+		if (nombre.length() >=3 && nombre.length() < 10){
+			this.nombre = nombre;
+		}
+	}
+	public boolean setVida(int vida){
+		boolean inicio = false;
+		if (vida > 0 && vida < 101 ){
+			this.vida = vida;
+			inicio = true;
+		}
+		return inicio;
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO " + nombre);
+	}
+	public void decVida(){
+		decVida(1);
+	}
+	public void decVida(int decrecer){
+		if((this.vida-decrecer) > 0){
+			this.vida -= decrecer; 
+		}
+	}
+}
