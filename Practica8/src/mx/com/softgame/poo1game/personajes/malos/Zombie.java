a79916d4a32e33e07957e4767308f80f513e04a0
AlineZavala-19 <79319893+AlineZavala-19@users.noreply.github.com>
2021-03-11 18:21:22 -0600

    Create Zombie.java

---
+++ Practica8/src/mx/com/softgame/poo1game/personajes/malos/Zombie.java
@@ -0,0 +1,27 @@
+package mx.com.softgame.poo1game.personajes.malos;
+import mx.com.softgame.poo1game.personajes.*;
+public class Zombie extends Personaje{
+	//atributos
+	boolean ataque;
+	//constructores
+	public Zombie(String nombre, int vida, boolean ataque){
+		super(nombre,vida);
+		this ataque = ataque;
+	}
+	public Zombie(String nombre, boolean ataque){
+		this(nombre,3,ataque);
+	}
+	public Zombie(String nombre){
+		this(nombre,3,false);
+	}
+	//metodos
+	public boolean getAtaque(){
+		return ataque;
+	}
+	public String getDetalle(){
+		return super.getDetalle() + "\t" + ataque;
+	}
+	public void decVida(int decrecer){
+		
+	}
+
