9afb0250fd405d9ae9c3cf4b8da8f4cece7ec6e7
AlineZavala-19 <79319893+AlineZavala-19@users.noreply.github.com>
2021-03-11 21:51:29 -0600

    Update Planta.java

--- Practica8/src/mx/com/softgame/poo1game/personajes/buenos/Planta.java
+++ Practica8/src/mx/com/softgame/poo1game/personajes/buenos/Planta.java
@@ -11,17 +11,17 @@ public class Planta extends Personaje{
 		super(nombre,vida);
 		this.escudo=escudo;
 	}
-	public Planta(String nombre, int vida, char escudo){
+	public Planta(String nombre, char escudo){
 		this(nombre,3,escudo);
 	}
-	public Planta(String nombre, char escudo){
+	public Planta(String nombre, int vida){
 		this(nombre,vida,'A');
 	}
 	public Planta(String nombre){
 		this(nombre,3,'A');
 	}
-	public getDetalle(){
-		return nombre + "\t" + vida + "\t" + escudo;
+	public String getDetalle(){
+		return super.getDetalle() + "\t" + escudo;
 	}
 	public void decVida(){
 		decVida(1);
