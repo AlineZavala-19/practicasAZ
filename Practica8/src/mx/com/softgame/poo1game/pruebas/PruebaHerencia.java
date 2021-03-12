ac0b9ca79871c1069368d78410fa223cf9ed08ec
AlineZavala-19 <79319893+AlineZavala-19@users.noreply.github.com>
2021-03-11 21:28:45 -0600

    Create PruebaHerencia.java

---
+++ Practica8/src/mx/com/softgame/poo1game/pruebas/PruebaHerencia.java
@@ -0,0 +1,36 @@
+package mx.com.softgame.poo1game.pruebas;
+import mx.com.softgame.poo1game.personajes.*;
+public class PruebaHerencia{
+	public static void main(String[] args){
+		Personaje per01 = new Personaje("Valeria",99);
+		Personaje per02 = new Personaje("Octavio");
+		Planta plan01 = new Planta("Wendy",10,'B');
+		Planta plan02 = new Planta("Ricardo",50);
+		Planta plan03 = new Planta("Vania",'C');
+		Planta plan04 = new Planta("Alan");
+		Zombie zom01 = new Zombie("Karen",88,false);
+		Zombie zom02 = new Zombie("Eduardo",true);
+		Zombie zom03 = new Zombie("Esmeralda");
+
+		Personaje arreglo[] = {per01,per02,plan01,plan02,plan03,plan04,zom01,zom02,zom03}
+
+		for (Personaje i: arreglo){
+			System.out.printl(i.getDetalle());
+			if (i instanceof Planta){
+				System.out.printl("Soy planta");
+			}
+			if (i instanceof Zombie){
+				System.out.printl("Soy zombie");
+			}
+			int decrecer = (int)(Math.random()*10);
+			i.decVida(decrecer);
+			System.out.printl(i.getDetalle());
+						if (i instanceof Planta){
+				System.out.printl("Soy planta");
+			}
+			if (i instanceof Zombie){
+				System.out.printl("Soy zombie");
+			}
+		}
+	}
+}
