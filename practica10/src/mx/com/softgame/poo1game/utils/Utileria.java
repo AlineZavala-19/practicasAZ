38e26e4151fda9363cadb526a5de662bd4475437
AlineZavala-19 <79319893+AlineZavala-19@users.noreply.github.com>
2021-03-19 08:32:03 -0600

    paso 5

--- practica10/src/mx/com/softgame/poo1game/utils/Utileria.java
+++ practica10/src/mx/com/softgame/poo1game/utils/Utileria.java
@@ -1,6 +1,6 @@
 package mx.com.softgame.poo1game.utils;
 public class Utileria{
-	public static int contadorID;
+	private static int contadorID;
 	private Utileria(){}//que nadie puede llamar al constructor de esta clase.
 
 	//metodos
