300c3875db7b3010061c202e9d431e3f22846433
AlineZavala-19 <79319893+AlineZavala-19@users.noreply.github.com>
2021-03-19 07:57:10 -0600

    hice privado el constructor

--- practica10/src/mx/com/softgame/poo1game/utils/Utileria.java
+++ practica10/src/mx/com/softgame/poo1game/utils/Utileria.java
@@ -1,6 +1,7 @@
 package mx.com.softgame.poo1game.utils;
 public class Utileria{
 	public static int contadorID;
+	private Utileria(){}//que nadie puede llamar al constructor de esta clase.
 
 	//metodos
 	public static int getID(){
