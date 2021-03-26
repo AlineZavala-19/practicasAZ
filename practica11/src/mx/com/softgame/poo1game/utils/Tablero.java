75ca0b6e9383abdccb3eb6bd229f5717a017e9cd
AlineZavala-19 <79319893+AlineZavala-19@users.noreply.github.com>
2021-03-26 08:50:56 -0600

    Paso 5

--- practica11/src/mx/com/softgame/poo1game/utils/Tablero.java
+++ practica11/src/mx/com/softgame/poo1game/utils/Tablero.java
@@ -1,5 +1,7 @@
 package mx.com.softgame.poo1game.utils;
+import mx.com.softgame.poo1game.personajes.*;
 public class Tablero{
 	public final static int MAX_LINE = 10;
 	private static int idx = -1;
+	private static Personaje[] personajes = {MAX_LINE};
 }
